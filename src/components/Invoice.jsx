282e63c763838fb90d0ce22dc4b46664dd1df434
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-11-17 21:30:37 +0530

    Added a cancel Btn

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -5,6 +5,7 @@ export default function MyModal({ setIsOpen, isOpen, onFormSubmit }) {
   function closeModal() {
     setIsOpen(false);
   }
+
   return (
     <Transition appear show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={closeModal}>
@@ -33,7 +34,7 @@ export default function MyModal({ setIsOpen, isOpen, onFormSubmit }) {
                 <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
                   Add Invoice
                 </Dialog.Title>
-                <form onSubmit={onFormSubmit}>
+                <form id="add-invoice" onSubmit={onFormSubmit} onReset={closeModal}>
                   <div className="mt-2">
                     <label className="block mb-2 text-sm font-medium text-gray-900 bg-transparent" htmlFor="file_input">
                       Upload file
@@ -51,9 +52,15 @@ export default function MyModal({ setIsOpen, isOpen, onFormSubmit }) {
 
                   <div className="mt-4 flex justify-center">
                     <button
+                      type="reset"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-yellow-100 px-4 py-2 text-sm font-medium text-yellow-700 hover:bg-yellow-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-500 focus-visible:ring-offset-2"
+                      >
+                      Cancel
+                    </button>
+                    <button
                       type="submit"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-700 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}>
+                      className="ml-4 inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-700 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2"
+                      >
                       Submit
                     </button>
                   </div>
